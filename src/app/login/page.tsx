3ad3b5daf6d9528199da4e66eec96eca9d88d3e2
speeaD <sejiofor20@gmail.com>
2026-03-23 11:17:20 +0100

    update

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import { Loader2, Mail } from 'lucide-react';
 
 export default function Login() {
-    const backend_url = process.env.BACKEND_URL || 'https://bjot-backend.vercel-nine.app/api';
+    const backend_url = process.env.BACKEND_URL || 'https://bjot-backend-nine.vercel.app/api';
     // const [showPassword, setShowPassword] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
     const [formData, setFormData] = useState({
